ad2433638d7952ebe028203767e8df02a001ed82
puthik Ma <puthik.ma@gmail.com>
2020-04-11 09:58:43 -0500

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,11 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +48,12 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
-
             app.UseRouting();
+            app.UseCors("CorsPolicy");
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers();
+            });
+          
 
             app.UseAuthorization();
 
